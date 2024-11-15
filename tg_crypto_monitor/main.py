ef55cc444792731b8b9e939be4b3caf0f675808a
Connor Swislow <52296992+ConnorSwis@users.noreply.github.com>
2024-11-15 14:25:30 -0500

    Update main.py

--- tg_crypto_monitor/main.py
+++ tg_crypto_monitor/main.py
@@ -4,7 +4,8 @@ from tg_crypto_monitor.config import PORT
 
 def main(reload: bool = False):
     """Main function to run the FastAPI app."""
-    uvicorn.run("telegram_channels_monitor:app", host="0.0.0.0", port=PORT, reload=reload)
+    uvicorn.run("tg_crypto_monitor:app", host="0.0.0.0",
+                port=PORT, reload=reload)
 
 
 main()
